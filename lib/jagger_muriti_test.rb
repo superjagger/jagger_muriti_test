c336cd8d65d6082bf70122d11f5854b93b227182
superjagger <31763819+superjagger@users.noreply.github.com>
2024-03-02 23:38:02 +0800

    Add files via upload

---
+++ lib/jagger_muriti_test.rb
@@ -0,0 +1,6 @@
+require "jagger_muriti_test/version"
+
+module JaggerMuritiTest
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/jagger_muriti_test/version.rb
@@ -0,0 +1,3 @@
+module JaggerMuritiTest
+  VERSION = "0.1.0"
+end
